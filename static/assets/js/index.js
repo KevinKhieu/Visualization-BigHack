64190e55dbfd3994e612fc1a01eeadc44a15d4db
Kevin Khieu <kkhieu@stanford.edu>
2016-04-09 01:59:47 -0700

    Google Maps + yeah

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,4 +1,14 @@
-var app = angular.module('refugee', ['ui.bootstrap']);
+var app = angular.module('refugee', ['ui.bootstrap', 'ngMap']);
+/*
+app.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyDmhHXfXkoT-lGtvDQgqn3TRXqUiVdMcJY',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})*/
 
 app.controller('RefugeeController', function ($scope, $sce) {
+	$scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    $scope.options = {scrollwheel: false};
 });
