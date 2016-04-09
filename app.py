b4d33a2e77de4ba447fe835c88cac031c4645623
Tum Chaturapruek <sorathan@cs.stanford.edu>
2016-04-09 01:07:53 -0700

    Add javacript stuff. Hook up to AngularJS.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+from flask import render_template
 from flask import Flask
+import json
+import random
+import re
+import requests
+from flask.ext.triangle import Triangle
+
 app = Flask(__name__)
+Triangle(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
