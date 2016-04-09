2f9880844ef98f9f48763c95681a9317b617a600
Tum Chaturapruek <sorathan@cs.stanford.edu>
2016-04-09 13:23:44 -0700

    Demo ready version.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from flask import Flask
+from flask import jsonify
 import json
 import random
 import re
 import requests
 from flask.ext.triangle import Triangle
+import csv
 
 app = Flask(__name__)
 Triangle(app)
@@ -16,6 +18,20 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/api/data')
+def data():
+    with open("static/data/volunteer-signups.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        results = []
+        for row in reader:
+            results.append(row)
+        final_results = {
+            "status": "success",
+            "data": results
+        }
+        return jsonify(**final_results)
+
 
 if __name__ == '__main__':
     app.debug = True
