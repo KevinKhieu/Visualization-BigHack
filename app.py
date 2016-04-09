742728b8b5773c7d458c3079467d9000674907f4
Tum Chaturapruek <sorathan@cs.stanford.edu>
2016-04-09 01:18:50 -0700

    Add navbar.

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ Triangle(app)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
