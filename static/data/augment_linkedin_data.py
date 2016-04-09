b196515325e3aba42b19dd12d060ad042808162d
Tum Chaturapruek <sorathan@cs.stanford.edu>
2016-04-09 16:55:40 -0700

    Add conversion script.

---
+++ static/data/augment_linkedin_data.py
@@ -0,0 +1,37 @@
+import csv
+import json
+linkedin_field = "If yes to either of the above, paste your LinkedIn below (or we can ask you for it later)"
+
+with open("volunteer-signups.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    headers = reader.fieldnames
+    augmented_header = headers.copy()
+    augmented_header.append("Skills")
+
+    enclosed_headers = []
+    for header in augmented_header:
+        enclosed_headers.append('"' + header + '"')
+
+    print(",".join(enclosed_headers))
+
+    results = []
+    for row in reader:
+        results.append(row)
+        to_print = []
+        for field in headers:
+            to_print.append('"' + row[field] + '"')
+
+        if "/in/" in row[linkedin_field]:
+            filename = row[linkedin_field].split("/in/")[-1]
+            with open("volunteers/" + filename) as profile:
+                things = profile.readlines()
+                things = "".join(things)
+                person = json.loads(things)
+                if person["skills"]:
+                    skills = ", ".join(filter(lambda x:x != "See less" and "See " not in x, person["skills"]))
+                    to_print.append('"' + skills + '"')
+                else:
+                    to_print.append("")
+        else:
+            to_print.append("")
+        print(",".join(to_print))
